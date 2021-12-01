21f758c47ac6bcebbfd7b83d4a28730cf28577da
Manutea Matthieu <mmatthie@c1r3p9.42nice.fr>
2021-12-01 19:36:41 +0100

    save

--- test/testprintf.c
+++ test/testprintf.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-	char	c;
 	char	*str;
 
 	str = "allo la terre\n";
-	c = 'c';
 	printf("____Printf____\n");
 	//ft_printf("ceci est le char %c", c);
 	ft_printf("ceci est le nb |%d|", c);
 	ft_printf("ceci est le str |%s|", str);
+	ft_printf("ceci est le x |%x|", &str);
+	printf("ceci est le x |%x|", &str);
 	printf("___end___\n");
 	return (0);
 }
