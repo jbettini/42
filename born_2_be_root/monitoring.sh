7db293e4b4234674f3deb887d3032702093f50a9
mmatthie98 <92974943+mmatthie98@users.noreply.github.com>
2021-12-15 13:08:34 +0100

    Delete born_2_be_root directory

--- born_2_be_root/monitoring.sh
+++
@@ -1,7 +0,0 @@
-arch = 'uname -a'
-cpu = 'grep -c ^processor /info/cpuinfo'
-memuse = 'free -m | grep 'Mem''
-diskuse = 'df / -h | grep /dev/sda1 //prend la ligne
-cpuload = ''
-lastreboot = 'who -b'
-lvmuse = ''
